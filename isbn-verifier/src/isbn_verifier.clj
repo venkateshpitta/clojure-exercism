47751cabc4eb1d0a99a4a69bbed06b802c9d6f81
Venkatesh Pitta <venkateshpitta@gmail.com>
2018-05-15 14:21:42 +1000

    First iteration of solution to isbn verify

--- isbn-verifier/src/isbn_verifier.clj
+++ isbn-verifier/src/isbn_verifier.clj
@@ -1,3 +1,16 @@
 (ns isbn-verifier)
 
-(defn isbn? [isbn])
+(defn isbn? [isbn]
+  (let [given (clojure.string/replace isbn "-" "")
+        final (get given (dec (count given)))]
+    (and (= (count given)
+            (count (.replaceAll given "[^0-9Xx]" "")))
+         (.contains [\0 \1 \2 \3 \4 \5 \6 \7 \8 \9 \x \X] final)
+         (every? (fn [x] (or (= \0 x) (= \1 x) (= \2 x) (= \3 x) (= \4 x) (= \5 x)
+                             (= \6 x) (= \7 x) (= \8 x) (= \9 x)))
+                 (rest (clojure.string/reverse given)))
+         (zero? (mod (reduce + (map (fn [x y] (* x (Integer/parseUnsignedInt y)))
+                                    (range 10 0 -1)
+                                    (map (fn [x] (if (= x "X") "10" x))
+                                         (clojure.string/split given #""))))
+                     11)))))
