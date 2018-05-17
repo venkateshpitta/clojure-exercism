ec9f7ae98363aa2298103709d015ef783149c068
Venkatesh Pitta <venkateshpitta@gmail.com>
2018-05-18 09:41:04 +1000

    First iteration of solution for collatz-conjecture

--- collatz-conjecture/src/collatz_conjecture.clj
+++ collatz-conjecture/src/collatz_conjecture.clj
@@ -1,3 +1,12 @@
 (ns collatz-conjecture)
 
-(defn collatz [n])
+(defn collatz [n]
+  (defn helper [m ret]
+    (if (<= m 0)
+      (throw (IllegalArgumentException. "IllegalArgumentException"))
+      (if (= m 1)
+        ret
+        (if (even? m)
+          (helper (/ m 2) (inc ret))
+          (helper (+ (* 3 m) 1) (inc ret))))))
+  (helper n 0))
