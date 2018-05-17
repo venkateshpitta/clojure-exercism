a48efe03ff61eee739f0d8319beb54af735eb389
Venkatesh Pitta <venkateshpitta@gmail.com>
2018-05-17 17:46:07 +1000

    First iteration of solution to say - from numbers to words

--- say/src/say.clj
+++ say/src/say.clj
@@ -1,5 +1,8 @@
-(ns say)
+(ns say
+  (:require [clojure.pprint]))
 
-(defn number [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn number [given]
+  (if (or (< given 0) (> given 999999999999))
+    (throw (IllegalArgumentException. "IllegalArgumentException"))
+    (clojure.string/replace (clojure.pprint/cl-format nil "~R" given)
+                            "," "")))
