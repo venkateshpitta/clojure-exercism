62446633b707571f9403efb4d8b04edeed3c85a0
Venkatesh Pitta <venkateshpitta@gmail.com>
2018-05-17 17:53:48 +1000

    Second iteration of solution for say - from numbers to words

--- say/src/say.clj
+++ say/src/say.clj
@@ -2,7 +2,7 @@
   (:require [clojure.pprint]))
 
 (defn number [given]
-  (if (or (< given 0) (> given 999999999999))
+  (if (not (< -1 given 999999999999))
     (throw (IllegalArgumentException. "IllegalArgumentException"))
     (clojure.string/replace (clojure.pprint/cl-format nil "~R" given)
                             "," "")))
