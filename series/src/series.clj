6209d761f16469f5cb7834643bd96e0c004bac3e
Venkatesh Pitta <venkateshpitta@gmail.com>
2018-05-18 10:39:12 +1000

    First iteration of solution for series

--- series/src/series.clj
+++ series/src/series.clj
@@ -1,3 +1,14 @@
 (ns series)
 
-(defn slices [string n])
+(defn slices [string n]
+  (cond
+    (empty? string) []
+    (zero? n) [""]
+    (> n (count string)) []
+    (= n (count string)) [string]
+    :else (loop [given string
+                 acc []]
+            (if (<= (count given) n)
+              (conj acc given)
+              (recur (apply str (drop 1 given))
+                     (conj acc (apply str (take 3 given))))))))
