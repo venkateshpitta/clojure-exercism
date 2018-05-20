6b7ed8dfb0cd8220ad7d12b47d6ee25b70828588
Venkatesh Pitta <venkateshpitta@gmail.com>
2018-05-21 00:27:17 +1000

    First iteration of solution to proverb.

--- proverb/src/proverb.clj
+++ proverb/src/proverb.clj
@@ -1,3 +1,20 @@
 (ns proverb)
 
-(def proverb "")
+(def proverb
+  (let [given ["nail" "shoe" "horse" "rider" "message" "battle" "kingdom"]
+        final-3 (reverse (take 3 given))
+        final-item (clojure.string/join " " [(clojure.string/join "" (take 2 final-3)) (last final-3)])
+        poem-proverb (loop [current given
+                            acc ""]
+                            (if (<= (count current) 1)
+                              (apply str (concat acc "\nAnd all for the want of a " final-item "."))
+                              (recur (rest current)
+                                     (clojure.string/join "\n"
+                                                          [acc
+                                                           (apply str
+                                                                  (concat "For want of a "
+                                                                          (first current)
+                                                                          " the "
+                                                                          (second current)
+                                                                          " was lost."))]))))]
+    (clojure.string/join "\n" (into [] (rest (clojure.string/split poem-proverb #"\n"))))))
