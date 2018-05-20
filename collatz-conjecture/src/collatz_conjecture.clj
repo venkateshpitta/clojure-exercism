cf696e6d1e008f586d25ae8570d9c4bc245c2a2e
Venkatesh Pitta <venkateshpitta@gmail.com>
2018-05-21 01:06:42 +1000

    Fourth iteration of solution to collatz-conjecture.

--- collatz-conjecture/src/collatz_conjecture.clj
+++ collatz-conjecture/src/collatz_conjecture.clj
@@ -3,11 +3,10 @@
 (defn collatz [n]
   (loop [m n
          ret 0]
-    (if (<= m 0)
-      (throw (IllegalArgumentException. "IllegalArgumentException"))
-      (if (= m 1)
-        ret
-        (recur (if (even? m)
-                  (/ m 2)
-                  (+ (* 3 m) 1))
-                (inc ret))))))
+    (cond
+      (<= m 0) (throw (IllegalArgumentException. ""))
+      (= m 1) ret
+      :else (recur (if (even? m)
+                     (/ m 2)
+                     (+ (* 3 m) 1))
+                   (inc ret)))))
